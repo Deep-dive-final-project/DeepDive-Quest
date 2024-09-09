1eeae7f04d00d0c26139ef919e9ed284fbd6cd35
koosco <rnxogud123@naver.com>
2024-09-09 13:47:33 +0900

    [refactor] 환경 변수 추가

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from config.cors.corsconfig import add_cors_config
 from repository import models
 from config.database.db_config import engine
 from config.router.router_config import init_router
+from dotenv import load_dotenv
+
 
 app = FastAPI()
 
+load_dotenv()
 add_cors_config(app)
 init_router(app)
 
